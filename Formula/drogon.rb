3d2bde26c1254bb51f6c438b7899ebf8dd3e5686
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-07-26 03:13:08 +0900

    Use git submodules to fetch `trantor`

--- Formula/drogon.rb
+++ Formula/drogon.rb
@@ -1,8 +1,10 @@
 class Drogon < Formula
   desc "HTTP(S) Web Application Framework (C++14/17 based)"
   homepage "https://drogon.docsforge.com"
-  url "https://github.com/drogonframework/drogon/archive/refs/tags/v1.7.2.tar.gz"
-  sha256 "a682fafc3619ba69d3d9278a80f6a386d1effcc3692b47e976de7cf9ff89d6b8"
+  # pull from git tag to get submodules
+  url "https://github.com/drogonframework/drogon.git",
+      tag:      "v1.7.2",
+      revision: "b68aeb43ae092c83e7e775c83c8ed7764418af0e"
   license "MIT"
   head "https://github.com/drogonframework/drogon.git"
 
@@ -14,15 +16,8 @@ class Drogon < Formula
   depends_on "ossp-uuid"
   depends_on "zlib"
 
-  resource "trantor" do
-    url "https://github.com/an-tao/trantor.git",
-        revision: "bef06c4f5e2dfc80303aee06a12770d09d5e8f5b"
-  end
-
   def install
     # TODO: Verify Parallelization - https://github.com/drogonframework/homebrew-drogon/issues/4
-    # head gets trantor as a git submodule
-    (buildpath/"trantor").install resource("trantor")
     system "cmake", "-B", "build", "-DOPENSSL_ROOT_DIR=#{Formula["openssl@1.1"].opt_prefix}", *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
